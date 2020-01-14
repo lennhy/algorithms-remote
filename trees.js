bb1c7ec2ffe25ddfd8fbf80d11c51d5c01b63181
lenn hypolite <lenn.hypolite@annalect.com>
2020-01-14 18:05:10 -0500

    Add a script for creating trees.

---
+++ trees.js
@@ -0,0 +1,28 @@
+class Node{
+	constructure(value){
+		this.value = null;
+		this.left = null;
+		this.right = null;
+	}
+} 
+
+class BinarySearchTree{
+	constructure(){
+		this.root = null
+	}
+
+	insert(value){
+		let node = new Node(val)
+		if(this.root === null){
+			this.root = node;
+			return this
+		}
+		else{
+
+		}
+	}
+}
+
+var = new BinarySearchTree()
+
+
